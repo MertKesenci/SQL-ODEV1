651528c5b87ca47835432b7f36c14c2e89875b52
Mert Kesenci <mertkesenci@gmail.com>
2022-02-23 21:11:36 +0300

    ödev1.sql dosyası güncellendi.

--- ödev1.sql
+++ ödev1.sql
@@ -3,7 +3,7 @@ SELECT title, description FROM film;
 
 --2) Film tablosunda bulunan tüm sütunlardaki verileri film uzunluğu (length) 60 dan büyük VE 75 ten küçük olma koşullarıyla sıralayınız. 
 SELECT * FROM film
-WHERE length > 60 and length < 75
+WHERE length > 60 and length < 75;
 
 --3) Film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99 VE replacement_cost 12.99 VEYA 28.99 olma koşullarıyla sıralayınız. 
 SELECT * FROM film
